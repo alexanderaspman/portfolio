00108c8c53feab8e3dc5d940ad1eb1f27602bedd
Alexander Aspman <aspmanalexander@gmail.com>
2020-10-25 23:53:18 +0100

    Update Portfolio.js
    sdsds

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,165 +1,82 @@
-
-import React, {Component} from 'react';
-
-import playStore from  '../images/playStore.jpg'
-import Vaskaorder from "../images/Vaskaorder1.png"
-import perdy from "../images/nannycutprofile.jpg"
-import Modal from "./Modal";
-import profil from '../images/profilbild.jpg'
-
-
-
-
-
-const images = [
-   '//www.flickr.com/photos/186003211@N03/49215989238/in/dateposted-public/',
-    '//www.flickr.com/photos/186003211@N03/49216724242/in/dateposted-public/'
-
-]
-
-class Portfolio extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            photoIndex: 0,
-            isOpen: false
-          };
-    }
-    state = {
-        showModal: 0
-      };
-    
-      getModal = value => {
-        this.setState({ showModal: value });
-      };
-    
-      hideModal = value => {
-        this.setState({ showModal: 0 });
-      };
-
-
-
-    render() {
-        const { photoIndex, isOpen } = this.state;
-
-       return( 
-        <section id="portfolio" >
-        <div className="container-fluid p-0">
-            <div className="row no-gutters">
-                <div className="col-lg-4 col-sm-6">
-                    <a className="portfolio-box" onClick={() => this.setState({ isOpen: true })}>
-                        <img className="img-fluid" src={Vaskaorder} alt="" />
-                        <div className="portfolio-box-caption">
-                            <div className="project-category text-white-50">
-                                Frontend:
-              </div>
-                            <div className="project-name">
-                                Online butik
-              </div>
-                        </div>
-                    </a>
-                  
-
-                </div>
-               {/*  {this.props.data.map((data, key) => (
-          <div key={key} className="small">
-            <p>Namsse: {data.name}</p>
-            <img className="img-fluid" onClick={() => this.getModal(data.id)} src={profil} alt="" />
-
-
-            <Modal
-              show={this.state.showModal === data.id}
-              onHide={() => this.hideModal(data.id)}
-              name={data.name}
-            />
-          </div>
-        ))} */}
-                <div className="col-lg-4 col-sm-6 ">
-                    <a className="portfolio-box" href="img/portfolio/fullsize/2.jpg">
-                        <img className="img-fluid" src={playStore} alt="" />
-                        <div className="portfolio-box-caption">
-                            <div className="project-category text-white-50">
-                                Frontend/Grafiskdesigner
-              </div>
-                            <div className="project-name">
-                                Spel webbsida
-              </div>
-                        </div>
-                    </a>
-                </div>
-                <div className="col-lg-4 col-sm-6">
-                    <a className="portfolio-box" href="img/portfolio/fullsize/3.jpg">
-                        <img className="img-fluid" src={perdy} alt="" />
-                        <div className="portfolio-box-caption">
-                            <div className="project-category text-white-50">
-                                Grafiskdesigner
-              </div>
-                            <div className="project-name">
-                            illustrator
-              </div>
-                        </div>
-                    </a>
-                </div>
-                <div className="col-lg-4 col-sm-6">
-                    <a className="portfolio-box" href="img/portfolio/fullsize/4.jpg">
-                        <img className="img-fluid" src="img/portfolio/thumbnails/4.jpg" alt="" />
-                        <div className="portfolio-box-caption">
-                            <div className="project-category text-white-50">
-                                Category
-              </div>
-                            <div className="project-name">
-                                Project Name
-              </div>
-                        </div>
-                    </a>
-                </div>
-                <div className="col-lg-4 col-sm-6">
-                    <a className="portfolio-box" href="#">
-                        <img className="img-fluid" src="#" alt="" />
-                        <div className="portfolio-box-caption">
-                            <div className="project-category text-white-50">
-                                Category
-              </div>
-                            <div className="project-name">
-                                Project Name
-              </div>
-                        </div>
-                    </a>
-                </div>
-                <div className="col-lg-4 col-sm-6">
-                    <a className="portfolio-box" href="img/portfolio/fullsize/6.jpg">
-                        <img className="img-fluid" src="img/portfolio/thumbnails/6.jpg" alt="" />
-                        <div className="portfolio-box-caption p-3">
-                            <div className="project-category text-white-50">
-                                Category
-              </div>
-                            <div className="project-name">
-                                Project Name
-              </div>
-                        </div>
-                    </a>
-                </div>
-            </div>
-        </div>
+import React from 'react';
+import $ from 'jquery';
+ 
+class Nav extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={
+        showMe:false,
+     };
+      
+      }
     
     
     
-     
-    </section>
-       
-       
-       
-       
-    )
+      operation(){
+    
+        this.setState({
+          showMe:! this.state.showMe
+      
+        });
+        
+      }
 
+  render() { 
+    
+  
+    
+  
 
-    }
+    return (<nav className="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+    <div className="container">
+      <a className="navbar-brand js-scroll-trigger" href="#top">På Toppen</a>
+      <button className="navbar-toggler navbar-toggler-right"  type="button" data-toggle="collapse" onClick={() =>  this.operation()} data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span  className="navbar-toggler-icon"></span>
+      </button>
+      {
+           this.state.showMe?
 
+           <div class="navbar-collapse" id="navbarResponsive">
+           <ul class="navbar-nav ml-auto my-2 my-lg-0">
+             <li class="nav-item">
+               <a class="nav-link js-scroll-trigger" href="#about">About</a>
+             </li>
+             <li class="nav-item">
+               <a class="nav-link js-scroll-trigger" href="#services">
+                   Skillz</a>
+             </li>
+             <li class="nav-item">
+               <a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
+             </li>
+             <li class="nav-item">
+               <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+             </li>
+           </ul>
+         </div>
+           :null}
 
+      <div className="collapse navbar-collapse" id="navbarResponsive">
+        <ul className="navbar-nav ml-auto my-2 my-lg-0">
+          <li className="nav-item">
+            <a className="nav-link js-scroll-trigger" href="#about">About</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link js-scroll-trigger" href="#services">
+                Job specialties</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link js-scroll-trigger" href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+      </div>
+    
+    
+    </nav>  );
+  }
 }
-
-
-
-    
-;
-export default Portfolio
+ 
+export default Nav
